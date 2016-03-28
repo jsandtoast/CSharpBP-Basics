ab3f5c2b81582a323653e65b276db429f2ee42eb
James S <jamessantosc@gmail.com>
2016-03-27 17:34:05 -0700

    Add Product class and a test for SayHello method

---
+++ AcmeApp/Acme.Biz/Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz
+{
+    /// <summary>
+    ///     Manages products carried in inventory.
+    /// </summary>
+    public class Product
+    {
+        public string ProductName { get; set; }
+
+        public string Description { get; set; }
+
+        public int ProductId { get; set; }
+
+        public string SayHello()
+        {
+            return "Hello " + ProductName + " (" + ProductId + "): " + Description;
+        }
+    }
+}
---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,28 @@
+﻿using Acme.Biz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Acme.BizTests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var currentProduct = new Product
+            {
+                ProductName = "Saw",
+                ProductId = 1,
+                Description = "15-inch steel blade hand saw"
+            };
+            var expected = "Hello Saw (1): 15-inch steel blade hand saw";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected,actual);
+        }
+    }
+}
